c9809ab2c76a52c6111c0538d8631f85e9fb67ab
bellebernal <asbernales@gmail.com>
2020-02-10 11:49:14 -0500

    removve unfinished search, update widescreen response

--- src/App.vue
+++ src/App.vue
@@ -12,41 +12,40 @@
       <v-img src="./assets/library-logo.svg"></v-img>
     </v-app-bar>
     <v-content>
-        <Search :search="search"
-                @search="handleSearch"/>
-        <!-- <Selector v-if="handleSearch" :selectorData="selectorData"/> -->
-        <Selector v-show="!handleSsearch" :selectorData="selectorData"/>
+      <!-- <Search :search="search" @search="handleSearch"/> -->
+      <!-- <Selector v-if="handleSearch" :selectorData="selectorData"/> -->
+      <Selector :selectorData="selectorData"/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Vue from 'vue';
-import { reactive, watch } from '@vue/composition-api';
-import Search from './components/Search.vue';
-import Selector from './components/Selector.vue';
-import axios from 'axios';
-import * as firebase from 'firebase/app';
+import Vue from "vue";
+import { reactive, watch } from "@vue/composition-api";
+import Search from "./components/Search.vue";
+import Selector from "./components/Selector.vue";
+import axios from "axios";
+import * as firebase from "firebase/app";
 
 const config = {
-    apiKey: "AIzaSyAzhYuONm41KLqw7Bed4-OPax2DkQQsFh0",
-    authDomain: "videos-db.firebaseapp.com",
-    databaseURL: "https://videos-db.firebaseio.com",
-    projectId: "videos-db",
-    storageBucket: "videos-db.appspot.com",
-    messagingSenderId: "119171243284",
-    appId: "1:119171243284:web:60b3210e49e55a2a5ab601"
+  apiKey: "AIzaSyAzhYuONm41KLqw7Bed4-OPax2DkQQsFh0",
+  authDomain: "videos-db.firebaseapp.com",
+  databaseURL: "https://videos-db.firebaseio.com",
+  projectId: "videos-db",
+  storageBucket: "videos-db.appspot.com",
+  messagingSenderId: "119171243284",
+  appId: "1:119171243284:web:60b3210e49e55a2a5ab601"
 };
 
 firebase.initializeApp(config);
 
 export default Vue.extend({
-  name: 'App',
+  name: "App",
   components: {
     Search,
     Selector
   },
-  props: ['seatch'],
+  props: ["search"],
   data() {
     return {
       // search,
@@ -58,39 +57,42 @@ export default Vue.extend({
   },
   methods: {
     handleSearch(searchItem) {
-        this.search = searchItem;
-      }
+      this.search = searchItem;
+    }
   },
   mounted() {
     axios
-      .get('https://videos-db.firebaseio.com/screens.json')
-      .then((response) => {  // then we take the response, search for config data to create the video cards, and add the views object to a separate videoData array 
+      .get("https://videos-db.firebaseio.com/screens.json")
+      .then(response => {
+        // then we take the response, search for config data to create the video cards, and add the views object to a separate videoData array
         this.videos = response.data;
         this.selectorData = this.videos[0].config.views;
-        this.videos.forEach((video) => {
-          this.videoData.push(video.config.views)
-        })
+        this.videos.forEach(video => {
+          this.videoData.push(video.config.views);
+        });
       })
-      .then(() => {  //  then search the videoData to find the actual player files we need and put them in a separate array
+      .then(() => {
+        //  then search the videoData to find the actual player files we need and put them in a separate array
         const playFiles = this.videoData;
-        playFiles.forEach((value,index) => {
-          if (playFiles.hasOwnProperty(index) && !isNaN(parseInt(index,10))) {
+        playFiles.forEach((value, index) => {
+          if (playFiles.hasOwnProperty(index) && !isNaN(parseInt(index, 10))) {
             value.filter(subarray => {
               const id = subarray.id;
-              if(id.includes('Player')) {
+              if (id.includes("Player")) {
                 this.playerData.push(subarray);
               }
             });
           }
-        })
+        });
       })
-      .then(() => {  // this block combines 2 arrays from above by creating a new files prop to each selector and assiging the corresponding play files
+      .then(() => {
+        // this block combines 2 arrays from above by creating a new files prop to each selector and assiging the corresponding play files
         const playables = this.playerData;
-        this.selectorData.forEach((selector) => {
-          selector['files']; 
-          playables.forEach((play) => { 
+        this.selectorData.forEach(selector => {
+          selector["files"];
+          playables.forEach(play => {
             if (play.id.includes(selector.id)) {
-              selector['files']= play.files;
+              selector["files"] = play.files;
             }
           });
         });
@@ -100,139 +102,133 @@ export default Vue.extend({
 </script>
 
 <style lang="scss">
+.v-app-bar {
+  display: flex;
+  height: 10%;
+  width: 100%;
+  align-items: center;
 
-  .v-app-bar {
-    display: flex;
-    height: 10%;
+  &.v-toolbar {
+    height: 9%;
     width: 100%;
-    align-items: center;
+  }
 
-    &.v-toolbar {
-        height: 9%;
-        width: 100%;
+  & .v-image {
+    margin-left: 10px;
+    height: 50px;
+    width: 50px;
+  }
+}
+
+.search {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 50%;
+  margin-left: 30%;
+
+  & .v-text-field {
+    // border: 2px solid darkcyan;
+    margin: 10px;
+    text-align: center;
+    font-weight: 500;
+    font-size: 1.2em;
+  }
+
+  & .v-btn {
+    width: 10%;
+    margin: 0px;
+    font-weight: 500;
+  }
+}
+
+.v-responsive__content {
+  display: flex;
+  justify-content: center;
+}
+
+.v-toolbar span {
+  color: darkcyan;
+  font-size: xx-large;
+}
+
+.v-content {
+  background: darkgray;
+  width: -webkit-fill-available;
+}
+
+a {
+  text-decoration: none;
+}
+
+#layout_row {
+  padding: 20px;
+  width: 100%;
+}
+
+.row {
+  justify-content: center;
+  flex-wrap: wrap;
+}
+
+.v-card {
+  text-decoration: none;
+  min-height: 100;
+  min-width: 100;
+  max-width: 200;
+  max-height: 200;
+
+  & .card-title {
+    color: orange;
+    font-weight: bolder;
+  }
+
+  &:hover {
+    border: 2px solid lightsalmon;
+    background: lightsalmon;
+    cursor: default;
+
+    .card-title {
+      color: darkcyan;
+      font-weight: bolder;
+    }
+  }
+}
+
+.v-btn span {
+  text-decoration: none;
+  color: darkcyan;
+}
+
+.v-icon.fa-window-close {
+  color: white;
+}
+
+input:focus,
+select:focus,
+textarea:focus,
+button:focus {
+  outline: none;
+}
+
+@media screen and (orientation: portrait) and (min-width: 320px) and (max-width: 589px) {
+  .v-app-bar {
+    &.v-toolbar span {
+      font-size: x-large;
+
+      &.v-toolbar__title {
+        font-size: medium;
+      }
     }
 
-    & .v-image {
-      margin-left: 10px;
-      height: 50px;
-      width: 50px;
+    &.v-image {
+      height: 20px;
+      width: 20px;
     }
-    
-  }
-  
-  .search {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    width: 50%;
-    margin-left: 30%;
-    
-    & .v-text-field {
-      // border: 2px solid darkcyan;
-      margin: 10px;
-      text-align: center;
-      font-weight: 500;
-      font-size: 1.2em;
-    }
-
-    & .v-btn {
-      width: 10%;
-      margin: 0px;
-      font-weight: 500;
-    }
-
-  }
-
-  .v-responsive__content {
-    display: flex;
-    justify-content: center;
-  }
-
-  .v-toolbar span {
-    color: darkcyan;
-    font-size: xx-large;
-  }
-
-  .v-content {
-    background: darkgray;
-    width: -webkit-fill-available;
-  }
-
-  a {
-    text-decoration: none;
   }
 
   #layout_row {
-    padding: 20px;
     width: 100%;
   }
-
-  .row {
-    justify-content: center;
-    flex-wrap: wrap;
-  }
-
-  .v-card {
-    text-decoration: none;
-    min-height: 100;
-    min-width: 100;
-    max-width: 200;
-    max-height: 200; 
-  
-
-    & .card-title {
-      color: orange;
-      font-weight: bolder;
-    }
-
-    &:hover {
-      border: 2px solid lightsalmon;
-      background:  lightsalmon;
-      cursor: default;
-
-        .card-title {
-          color: darkcyan;
-          font-weight: bolder;
-        }
-    }
-  }
-
-  .v-btn span{
-    text-decoration: none;
-    color: darkcyan;
-  }
-
-  .v-icon.fa-window-close {
-      color: white;
-  }
-
-  input:focus,
-  select:focus,
-  textarea:focus,
-  button:focus {
-    outline: none;
-  }
-
-  @media screen and (orientation: portrait) and (min-width: 320px) and (max-width: 589px) {
-    .v-app-bar {
-
-      &.v-toolbar span {
-        font-size: x-large;
-
-        &.v-toolbar__title {
-          font-size: medium;
-        }
-      }
-      
-      &.v-image {
-        height: 20px;
-        width: 20px;
-      }
-    }
-
-    #layout_row {
-      width: 100%;
-    }
-  }
-
+}
 </style>
--- src/components/Selector.vue
+++ src/components/Selector.vue
@@ -1,17 +1,9 @@
 <template>
   <v-layout row wrap id="layout_row">
-    <v-flex 
-      xs8 sm4 md3 lg3 xl1
-      v-for="video in selectorData"
-      :key="video.id">
+    <v-flex xs8 sm4 md3 lg3 xl2 v-for="video in selectorData" :key="video.id">
       <v-card class="ma-3" elevation="15">
         <v-layout column fill-height class="text-left">
-          <v-img
-            :video="video"
-            class="orange--text"
-            fill-height
-            :src="video.thumbnail">
-          </v-img>
+          <v-img :video="video" class="orange--text" fill-height :src="video.thumbnail"></v-img>
           <v-card-title class="card-title">{{ video.title }}</v-card-title>
           <v-card-text>
             <span class="text--primary">
@@ -21,13 +13,10 @@
             </span>
           </v-card-text>
           <v-card-actions>
-              <v-btn
-                class="orange"
-                @click="addToPlayer(video)"
-                >
-                <v-icon small>fas fa-play</v-icon>
-                <span id="play">play</span>
-              </v-btn>
+            <v-btn class="orange" @click="addToPlayer(video)">
+              <v-icon small>fas fa-play</v-icon>
+              <span id="play">play</span>
+            </v-btn>
           </v-card-actions>
         </v-layout>
       </v-card>
@@ -41,12 +30,12 @@
 
 
 <script>
-import Player from './Player.vue'
+import Player from "./Player.vue";
 
 export default {
-  name: 'Selector',
+  name: "Selector",
   components: { Player },
-  props:['selectorData'],
+  props: ["selectorData"],
   data() {
     return {
       active: [],
@@ -59,42 +48,39 @@ export default {
     },
     display() {
       if (this.active.length > 0) {
-      document.getElementById('play').addEventListener('click',
-          function() {
-              document.getElementById('player').style.display='flex';
-          });
+        document.getElementById("play").addEventListener("click", function() {
+          document.getElementById("player").style.display = "flex";
+        });
       }
     }
   },
   methods: {
     addToPlayer(video) {
-      this.componentKey += 1; 
-      this.active = [];  // resets active array state to empty
+      this.componentKey += 1;
+      this.active = []; // resets active array state to empty
       this.active = video.files; // then, assigns the current selected video's files
-      this.$emit('update-video', this.active);
+      this.$emit("update-video", this.active);
     }
   }
 };
 </script>
 
 <style>
-
-  .video-player {
-    box-sizing: border-box;
-    background-size: cover;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    min-height: 100vh;
-    margin: 0;
-    padding: 0;
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 10;
-    background-color: rgba(0,0,0,0.5);
+.video-player {
+  box-sizing: border-box;
+  background-size: cover;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0;
+  padding: 0;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 10;
+  background-color: rgba(0, 0, 0, 0.5);
 }
-
 </style>
